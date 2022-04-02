5a980bf4bf36459788d8a5546098255b0909c9eb
Schyler27 <92312027+Schyler27@users.noreply.github.com>
2022-04-02 15:26:59 +0530

    Add files via upload

---
+++ main.js
@@ -0,0 +1,40 @@
+mustache_X = 0;
+mustache_Y = 0;
+
+function preload(){
+    mustache = loadImage('https://i.postimg.cc/K82ncc17/m.png')
+}
+
+function setup(){
+    canvas = createCanvas(300 , 300);
+    canvas.center();
+    video = createCapture(VIDEO);
+    video.size(300 , 300);
+    video.hide()
+
+    poseNet = ml5.poseNet(video , modelLoaded);
+    poseNet.on('pose' , gotPoses);
+}
+
+function modelLoaded(){
+    console.log("PoseNet is Intialized");
+}
+
+function gotPoses(results){
+    if(results.length > 0){
+        console.log(results);
+        mustache_X = results[0].pose.nose.x-10;
+        mustache_Y = results[0].pose.nose.y;
+        console.log("mustache x = " + results[0].pose.nose.x);
+        console.log("mustache y = " + results[0].pose.nose.y); 
+    }
+}
+
+function draw(){
+    image(video , 0 , 0 , 300 , 300);
+    image(mustache , mustache_X , mustache_Y , 30 , 30);
+}
+
+function takesnapshot(){
+    save("MyFilter.png");
+}
